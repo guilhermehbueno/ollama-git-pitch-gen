589520b15322d21ec5380711cb4af136001d65fa
Guilherme Henrique Bueno <guilhermehbueno@gmail.com>
2025-02-08 21:25:24 -0300

    📄 Model: git-assistant fix: Clean up code for processing commit message comments (#123)

--- prepare-commit-msg.sh
+++ prepare-commit-msg.sh
@@ -106,7 +106,6 @@ process_commit_message() {
 
     # Read the message line by line
     while IFS= read -r line; do
-        echo "Line: $line"
         if [[ "$line" =~ "<think>" ]]; then
             inside_think=1
             think_comment+="# THINK: ${line//<think>/}\n"  # Start comment, remove <think>
